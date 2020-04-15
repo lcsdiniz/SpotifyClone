b93d677c5f408bd2cd0788c9d97c05a79be0215b
lcsdiniz <lucasdiniz.smt@gmail.com>
2020-04-15 20:22:24 -0300

    Back-end

---
+++ config/routes.rb
@@ -0,0 +1,29 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+  root 'home#index'
+
+  concern :favoritable do |options|
+    shallow do
+      post "/favorite", { to: "favorites#create", on: :member }.merge(options)
+      delete "/favorite", { to: "favorites#destroy", on: :member }.merge(options)
+    end
+  end
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :dashboard, only: :index
+      resources :search, only: :index
+      resources :categories, only: %i[index show]
+      resources :albums, only: :show do
+        resources :recently_heards, only: :create
+      end
+
+      resources :favorites, only: :index  
+
+      resources :songs, only: [] do
+        concerns :favoritable, favoritable_type: 'Song'
+      end
+    end
+  end
+end
